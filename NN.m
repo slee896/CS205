d2177e2b0daf7b5f32d4d9e0b52cdfc2347e487b
slee896 <91647731+slee896@users.noreply.github.com>
2022-05-29 11:09:45 -0700

    Update NN.m

--- NN.m
+++ NN.m
@@ -1,6 +1,5 @@
-% NN(Nearest_neighbor) algorithm (9 lines)
-% For 1 feature, For 1 test_point with 
-function nearest_neighbor_class = NN(test_point, test_set) % return: nearest_vector
-diff = test_point(1, 2:end)-test_set(:, 2:end);
-[square_root_r,index] = sort(sqrt(sum(sqrt(diff.*diff),2)));
+% NN(Nearest_neighbor) algorithm  
+function nearest_neighbor_class = NN(test_point, test_set)   % return: nearest_class
+diff = test_point(1, 2:end)-test_set(:, 2:end);              
+[square_root_r,index] = sort(sqrt(sum(sqrt(diff.*diff),2))); % Euclidean distance
 nearest_neighbor_class = test_set(index(2),1);
