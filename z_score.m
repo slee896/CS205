f7ecd542165bc53dd97761b97fe9416edb0f2746
slee896 <91647731+slee896@users.noreply.github.com>
2022-05-30 14:27:13 -0700

    Add files via upload

---
+++ z_score.m
@@ -0,0 +1,5 @@
+function z_score_data = z_score(data)
+n = size(data,1)*size(data,2);              % # of elements
+m = mean(data, 'all');                      % mean
+s = sqrt(sum((data-m).*(data-m),'all')/n);  % standard deviation
+z_score_data = (data-m)/s                   % z_score = (x-μ) / σ
