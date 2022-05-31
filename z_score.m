1c683567780eae3d52029051e685fb5e1a9adf7d
slee896 <91647731+slee896@users.noreply.github.com>
2022-05-30 17:35:11 -0700

    Update z_score.m

--- z_score.m
+++ z_score.m
@@ -2,4 +2,4 @@ function z_score_data = z_score(data)
 n = size(data,1)*size(data,2);              % # of elements
 m = mean(data, 'all');                      % mean
 s = sqrt(sum((data-m).*(data-m),'all')/n);  % standard deviation
-z_score_data = (data-m)/s                   % z_score = (x-μ) / σ
+z_score_data = (data-m)/s;                  % z_score = (x-μ) / σ
